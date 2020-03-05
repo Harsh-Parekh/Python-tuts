4bee1a62c9d1bfb2916709cc7e13b177cc3b0160
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-03-05 22:14:13 +0530

    Add files via upload
    Pythons data-structures
    1>list
    2>tuples
    3>set
    4>dictionary

---
+++ Data-structure-python/List.py
@@ -0,0 +1,25 @@
+ls=[1,1,"Asit","Harshal",'N',45.5,45.5]
+
+print(ls)
+#[1, 1, 'Asit', 'Harshal', 'N', 45.5, 45.5]
+
+print(ls[0])
+#1
+
+print(ls[0:2])
+#[1, 1]
+
+print(ls[0:])
+#[1, 1, 'Asit', 'Harshal', 'N', 45.5, 45.5]
+
+ls[5]=66
+
+print(ls)
+#[1, 1, 'Asit', 'Harshal', 'N', 66, 45.5]
+
+ls.append("Harsh")
+print(ls)
+#[1, 1, 'Asit', 'Harshal', 'N', 66, 45.5, 'Harsh']
+
+print(type(ls))
+#<class 'list'>
