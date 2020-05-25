54688bd139d0ef3a91dcdb9cb40cb51d36f39e73
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-05-25 22:03:17 +0530

    Update Tuple.py

--- Data-structure-python/Tuple.py
+++ Data-structure-python/Tuple.py
@@ -1,7 +1,7 @@
-tupl=(1,1,"Asit","Harshal",'N',45.5,45.5)
+tupl=(1,1,"Asit","Harsh",'N',45.5,45.5)
 
 print(tupl)
-#[1, 1, 'Asit', 'Harshal', 'N', 45.5, 45.5]
+#[1, 1, 'Asit', 'Harsh', 'N', 45.5, 45.5]
 
 print(tupl[0])
 #1
@@ -10,13 +10,13 @@ print(tupl[0:2])
 #[1, 1]
 
 print(tupl[0:])
-#[1, 1, 'Asit', 'Harshal', 'N', 45.5, 45.5]
+#[1, 1, 'Asit', 'Harsh', 'N', 45.5, 45.5]
 
 #tupl[5]=66
 #'tuple' object does not support item assignment
 
 print(tupl)
-#[1, 1, 'Asit', 'Harshal', 'N', 66, 45.5]
+#[1, 1, 'Asit', 'Harsh', 'N', 66, 45.5]
 
 print(type(tupl))
 #<class 'list'>
