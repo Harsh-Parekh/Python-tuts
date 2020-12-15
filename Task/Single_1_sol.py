dace20c09310b9d10786f322cf5f675ae36b1806
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-12-15 10:04:06 +0530

    Update Single_1_sol.py

--- Task/Single_1_sol.py
+++ Task/Single_1_sol.py
@@ -1,13 +1,18 @@
-nums = [1,1,2,3,3,4,4,8,8]
 class Sol:
     def Single(self,list):
+        ret_ls=[]
         for i in list:
             counter=nums.count(i)
             if counter==1:
-                return i
-            else:
-                return -1
+                ret_ls.append(i)
+        if len(ret_ls)==0:
+            return -1
+        else:
+            return ret_ls
+            
+nums=list(map(int,input('Enter list by space :').split()))
 
 s=Sol()
 ret_val=s.Single(nums)
-print(ret_val)
+
+print("The singel element present in list are :",ret_val)
